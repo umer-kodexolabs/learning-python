9a51dc700eb420791f59b4b250d22b41fc9c1b92
umer-kodexolabs <muhammad.umer@kodexolabs.com>
2025-04-07 21:45:46 +0500

    chore: add basics.py

---
+++ basic.py
@@ -0,0 +1,74 @@
+# Math Operators
+print((2 + 3) * 6)  # 30
+print(2 + 3 * 6)  # 20
+print(2**8)  # 256  (exponentiation)
+
+print((2 + 3) * 6)  # 30
+print(23 // 7)  # 3 (integer division)
+print(23 % 7)  # 2 (modulus)
+
+
+# comments
+
+# Single line comment
+# This is a single line comment
+
+# Multi-line comment
+"""
+This is a multi-line comment
+This is a multi-line comment
+"""
+
+
+# Variables
+# Variables can even change type after they have been set.
+x = 5
+x = "Hello"
+print(x)  # Hello
+
+# Type Casting
+a = int(5)  # int
+b = float(5)  # float
+c = str(5)  # string
+print(type(a))  # 5
+print(type(b))  # 5.0
+print(type(c))  # 5
+# String Formatting
+name = "John"
+age = 30
+# f-string formatting
+print(f"My name is {name} and I am {age} years old.")
+
+# Python Keywords (33 keywords)
+# as ----->	To create an alias
+# assert ----->	For debugging
+# break ----->	To break out of a loop
+# class ----->	To define a class
+# continue ----->	To continue to the next iteration of a loop
+# def ----->	To define a function
+# del ----->	To delete an object
+# elif ----->	Used in conditional statements, same as else if
+# else ----->	Used in conditional statements
+# except ----->	Used with exceptions, what to do when an exception occurs
+# False	Boolean -----> value, result of comparison operations
+# finally ----->	Used with exceptions, a block of code that will be executed no matter if there is an exception or not
+# for ----->	To create a for loop
+# from ----->	To import specific parts of a module
+# global ----->	To declare a global variable
+# if ----->	To make a conditional statement
+# import ----->	To import a module
+# in ----->	To check if a value is present in a list, tuple, etc.
+# is ----->	To test if two variables are equal
+# lambda ----->	To create an anonymous function
+# None ----->	Represents a null value
+# nonlocal ----->	To declare a non-local variable
+# not ----->	A logical operator
+# or ----->	A logical operator
+# pass ----->	A null statement, a statement that will do nothing
+# raise ----->	To raise an exception
+# return ----->	To exit a function and return a value
+# True ----->	Boolean value, result of comparison operations
+# try ----->	To make a try...except statement
+# while ----->	To create a while loop
+# with ----->	Used to simplify exception handling
+# yield ----->	To return a list of values from a generator
